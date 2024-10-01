c0465da920b8af8ab7ad99725d120eba324adede
dagergit <jklhian112@gmail.com>
2024-10-01 17:58:30 -0500

    se agrega al html las pregunta de la tarea 3 y ultimo punto de js

--- luis-dager-3148471107/tarea-3/main.js
+++ luis-dager-3148471107/tarea-3/main.js
@@ -1,5 +1,8 @@
 let number = 1;
 
 for (let number = 1; number <= 2000; number++) {
-  console.log(number);
+  const numStr = String(number);
+  if (numStr.startsWith("2")) {
+    console.log(numStr);
+  }
 }
