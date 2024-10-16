d457cc4199d73a566c875d473c4c72fe66e5a733
David-1707 <davidarboleda07@gmail.com>
2024-10-16 17:24:12 -0500

    Subo tarea 6 con sus respectivas actividades

---
+++ david-arboleda-3152900014/tarea-6/frutas.js
@@ -0,0 +1,8 @@
+let frutas1 = ["manzana", "platano", "naranja"];
+let frutas2 = ["fresa", "uva", "piña"];
+
+for (i = 0; i < frutas2.length; i++) {
+    frutas1.push(frutas2[i]); 
+} 
+  console.log("frutas concatenadas:");
+  console.log(frutas1);
