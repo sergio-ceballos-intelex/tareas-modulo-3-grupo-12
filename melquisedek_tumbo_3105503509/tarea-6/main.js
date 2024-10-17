e4ca992ae3697ccf72bda75f8b0abb10d3e73339
[Melquisedek Tumbo] <tmelquisedek11@gmail.com>
2024-10-16 22:05:42 -0500

    se creo un scrip donde busca en un array de productos, el producto de menor valor

--- melquisedek_tumbo_3105503509/tarea-6/main.js
+++ melquisedek_tumbo_3105503509/tarea-6/main.js
@@ -1,13 +1,42 @@
+// 1.
 // Dado el siguiente array de números:
 
 // let numeros = [1, 2, 3, 4, 5];
 // Utiliza un bucle for para multiplicar cada número por 2 y almacenar el resultado en un nuevo array. Luego, imprime el nuevo array.
-const numerosx2 = []
-let numeros = [1, 2, 3, 4, 5];
+// const numerosx2 = []
+// let numeros = [1, 2, 3, 4, 5];
 
-for(let i = 0; i < numeros.length; i++ ){
-    const element = numeros[i] * 2
-    numerosx2.push(element)
-}
+// for(let i = 0; i < numeros.length; i++ ){
+//     const element = numeros[i] * 2
+//     numerosx2.push(element)
+// }
 
-console.log(numerosx2)
+
+
+// console.log(numerosx2)
+
+// 2.
+// Dado el siguiente array de objetos que representan productos:
+
+// let productos = [
+//   { nombre: "Camisa", precio: 20 },
+//   { nombre: "Pantalón", precio: 30 },
+//   { nombre: "Zapatos", precio: 50 },
+// ];
+// Utiliza un bucle for para encontrar el producto con el precio más bajo. Luego, imprime el nombre y el precio de ese producto.
+
+let productos = [
+      { nombre: "Camisa", precio: 20 },
+      { nombre: "Pantalón", precio: 30 },
+      { nombre: "Zapatos", precio: 50 },
+    ];
+
+    let propreciomenor = productos[0];
+    
+  for ( let i=0; i < productos.length; i++){
+    if (productos[i].precio < propreciomenor.precio) {
+        propreciomenor = productos[i]; 
+      }
+  }
+  
+  console.log(propreciomenor)
