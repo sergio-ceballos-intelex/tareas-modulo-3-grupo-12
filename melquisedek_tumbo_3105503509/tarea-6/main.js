f26b50cba12e9557a44300490338f0ae04e802fc
[Melquisedek Tumbo] <tmelquisedek11@gmail.com>
2024-10-16 22:55:39 -0500

    se creo un scrip donde en un array de objetos de libros,se debe encontrar los libros que el titulo tenga la palabra el

--- melquisedek_tumbo_3105503509/tarea-6/main.js
+++ melquisedek_tumbo_3105503509/tarea-6/main.js
@@ -47,14 +47,42 @@
 // ];
 // Utiliza un bucle for para incrementar la edad de cada estudiante en 1 año. Luego, imprime el array modificado.
 
-let estudiantes = [
-      { nombre: "Juan", edad: 20 },
-      { nombre: "María", edad: 22 },
-      { nombre: "Pedro", edad: 19 },
+// let estudiantes = [
+//       { nombre: "Juan", edad: 20 },
+//       { nombre: "María", edad: 22 },
+//       { nombre: "Pedro", edad: 19 },
+//     ];
+
+//     for(let i = 0; i < estudiantes.length; i++){
+//           estudiantes [i].edad = estudiantes[i].edad +1
+//     }
+
+//     console.log(estudiantes)
+
+
+// Dado el siguiente array de objetos que representan libros:
+
+// let libros = [
+//   { titulo: "El principito", autor: "Antoine de Saint-Exupéry" },
+//   { titulo: "Don Quijote de la Mancha", autor: "Miguel de Cervantes" },
+//   { titulo: "Cien años de soledad", autor: "Gabriel García Márquez" },
+// ];
+// Utiliza un bucle for para encontrar todos los libros cuyo título contenga la palabra "El". Luego, imprime estos libros.
+
+
+let libros = [
+      { titulo: "El principito", autor: "Antoine de Saint-Exupéry" },
+      { titulo: "Don Quijote de la Mancha", autor: "Miguel de Cervantes" },
+      { titulo: "Cien años de soledad", autor: "Gabriel García Márquez" },
     ];
 
-    for(let i = 0; i < estudiantes.length; i++){
-          estudiantes [i].edad = estudiantes[i].edad +1
-    }
 
-    console.log(estudiantes)
+    let librosConEl = [];
+
+for (i = 0; i < libros.length; i++) {
+  if (libros[i].titulo.startsWith("El ")) { 
+    librosConEl.push(libros[i]);
+  }
+}
+
+console.log(librosConEl);
