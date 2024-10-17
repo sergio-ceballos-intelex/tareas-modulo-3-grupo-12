defb1436b16cca061f709c1b6d0d4a0213636912
[Melquisedek Tumbo] <tmelquisedek11@gmail.com>
2024-10-16 22:34:12 -0500

    se creo un scrip donde en un array de objetos de estudiantes, se incrementa a cada estudiante su edad en 1 año

--- melquisedek_tumbo_3105503509/tarea-6/main.js
+++ melquisedek_tumbo_3105503509/tarea-6/main.js
@@ -11,8 +11,6 @@
 //     numerosx2.push(element)
 // }
 
-
-
 // console.log(numerosx2)
 
 // 2.
@@ -25,18 +23,38 @@
 // ];
 // Utiliza un bucle for para encontrar el producto con el precio más bajo. Luego, imprime el nombre y el precio de ese producto.
 
-let productos = [
-      { nombre: "Camisa", precio: 20 },
-      { nombre: "Pantalón", precio: 30 },
-      { nombre: "Zapatos", precio: 50 },
+// let productos = [
+//       { nombre: "Camisa", precio: 20 },
+//       { nombre: "Pantalón", precio: 30 },
+//       { nombre: "Zapatos", precio: 50 },
+//     ];
+
+//     let propreciomenor = productos[0];
+    
+//   for ( let i=0; i < productos.length; i++){
+//     if (productos[i].precio < propreciomenor.precio) {
+//         propreciomenor = productos[i]; 
+//       }
+//   }
+
+//   console.log(propreciomenor) 
+// Dado el siguiente array de objetos que representan estudiantes:
+
+// let estudiantes = [
+//   { nombre: "Juan", edad: 20 },
+//   { nombre: "María", edad: 22 },
+//   { nombre: "Pedro", edad: 19 },
+// ];
+// Utiliza un bucle for para incrementar la edad de cada estudiante en 1 año. Luego, imprime el array modificado.
+
+let estudiantes = [
+      { nombre: "Juan", edad: 20 },
+      { nombre: "María", edad: 22 },
+      { nombre: "Pedro", edad: 19 },
     ];
 
-    let propreciomenor = productos[0];
-    
-  for ( let i=0; i < productos.length; i++){
-    if (productos[i].precio < propreciomenor.precio) {
-        propreciomenor = productos[i]; 
-      }
-  }
-  
-  console.log(propreciomenor)
+    for(let i = 0; i < estudiantes.length; i++){
+          estudiantes [i].edad = estudiantes[i].edad +1
+    }
+
+    console.log(estudiantes)
