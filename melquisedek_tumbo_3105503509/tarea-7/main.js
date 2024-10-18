ea0e5b9343dee9c07691582522a05f0797cfcc3c
[Melquisedek Tumbo] <tmelquisedek11@gmail.com>
2024-10-18 18:14:30 -0500

    se resolvio el punto 4: busqueda y filtrado de datos

--- melquisedek_tumbo_3105503509/tarea-7/main.js
+++ melquisedek_tumbo_3105503509/tarea-7/main.js
@@ -51,23 +51,104 @@
 
 // Punto 3: Cálculo de Estadísticas Básicas: Calcula la suma de las edades de todos los estudiantes en el array (puedes utilizar un map o foreach).
 
-const estudiantes = [
-          { nombre: "Camilo", edad: 33, promedio: 10 },
-          { nombre: "Juan", edad: 30, promedio: 6 },
-          { nombre: "Andres", edad: 29, promedio: 8.7 },
-     ];
+// const estudiantes = [
+//           { nombre: "Camilo", edad: 33, promedio: 10 },
+//           { nombre: "Juan", edad: 30, promedio: 6 },
+//           { nombre: "Andres", edad: 29, promedio: 8.7 },
+//      ];
 
-// Calcula el promedio de edad de los estudiantes.
-let sumatotal = 0;
-const sumapromedio = estudiantes.map(suma => {
-    sumatotal += suma.promedio
+// // Calcula el promedio de edad de los estudiantes.
+// let sumatotal = 0;
+// const sumapromedio = estudiantes.map(suma => {
+//     sumatotal += suma.promedio
     
-})
+// })
 
-const promediototalstudiantes = sumatotal / estudiantes.length
-// Imprime en la consola tanto la suma como el promedio de edad de los estudiantes.
+// const promediototalstudiantes = sumatotal / estudiantes.length
+// // Imprime en la consola tanto la suma como el promedio de edad de los estudiantes.
 
 
-console.log(sumatotal)
-console.log(promediototalstudiantes.toFixed(2))
+// console.log(sumatotal)
+// console.log(promediototalstudiantes.toFixed(2))
 
+// -------------------------------------------------------------------------------------------------------
+
+// Punto 4: Búsqueda y Filtrado de Datos: Utiliza filter para encontrar al estudiante con el promedio más alto en el array.
+
+// Imprime en la consola el nombre y la edad del estudiante con el promedio más alto.
+// Filtra los estudiantes mayores de 20 años en un nuevo array llamado estudiantesMayores.
+// Imprime en la consola la información completa de los estudiantes mayores de 20 años.
+
+const estudiantes = [
+  {
+    nombre: "Ana",
+    edad: 20,
+    promedio: 85,
+    genero: "Femenino",
+    carrera: "Ingeniería Civil",
+    ciudad: "Bogotá",
+  },
+  {
+    nombre: "Juan",
+    edad: 22,
+    promedio: 78,
+    genero: "Masculino",
+    carrera: "Medicina",
+    ciudad: "Medellín",
+  },
+  {
+    nombre: "María",
+    edad: 21,
+    promedio: 90,
+    genero: "Femenino",
+    carrera: "Administración de Empresas",
+    ciudad: "Cali",
+  },
+  {
+    nombre: "Pedro",
+    edad: 23,
+    promedio: 82,
+    genero: "Masculino",
+    carrera: "Derecho",
+    ciudad: "Barranquilla",
+  },
+  {
+    nombre: "Laura",
+    edad: 24,
+    promedio: 88,
+    genero: "Femenino",
+    carrera: "Arquitectura",
+    ciudad: "Cartagena",
+  },
+  {
+    nombre: "Carlos",
+    edad: 20,
+    promedio: 75,
+    genero: "Masculino",
+    carrera: "Ingeniería de Sistemas",
+    ciudad: "Santa Marta",
+  },
+  {
+    nombre: "Sofía",
+    edad: 22,
+    promedio: 95,
+    genero: "Femenino",
+    carrera: "Psicología",
+    ciudad: "Pereira",
+  },
+];
+
+
+const promediomasalto = estudiantes.filter(promedio => promedio.promedio === Math.max(...estudiantes
+.map(estudiantes => estudiantes.promedio)));
+
+// console.log(promediomasalto)
+
+
+
+const estudiantesMayores = [];
+
+const studentsmayores = estudiantes.filter(estudiantes => estudiantes.edad > 20)
+estudiantesMayores.push(studentsmayores)
+
+console.log(estudiantesMayores)
